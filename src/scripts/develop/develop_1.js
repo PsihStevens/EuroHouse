d13e44191952ab77c56aed1f8514419af1dcb620
Artem Lopatovskiy <herzz86@gmail.com>
2017-07-29 21:22:16 +0300

    home page

--- src/scripts/develop/develop_1.js
+++ src/scripts/develop/develop_1.js
@@ -3,6 +3,16 @@
 
 $(document).ready(function(){
 
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        smartSpeed: 1500,
+        loop: true,
+        autoWidth:true,
+        // autoHeight:true
+    });
+
 });
 
 $(window).load(function(){
